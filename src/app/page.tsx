a17151395c31898f8f3491d5bd862453f6c4ea95
Aditya Vaish <adityavaish846@gmail.com>
2025-06-07 22:08:43 -0700

    changes made to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 /*OSC-Lit*/
 export default function Home() {
   return (
-    <div>
-      
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">In Production...</h1>
     </div>
   );
 }
