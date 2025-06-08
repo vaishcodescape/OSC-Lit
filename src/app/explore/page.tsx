96039d1a722b3fdb7747b55968ebd86a0a764af8
Aditya Vaish <adityavaish846@gmail.com>
2025-06-08 14:06:40 -0700

    pages crafted

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+export default function Explore()
+{
+    return(
+        
+        <div>
+            <h1>Explore Repositories</h1>
+        </div>
+    
+    )
+}
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+export default function Signin()
+{
+    return(
+    <div>
+    <h1>Welcome to the Signin page</h1>
+    </div>
+    )
+};
